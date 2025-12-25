b8c9c8e72afbfe29e9ee98c03f2833b172e40c43
Dennis Macapagal <macapagal.dennis@gmail.com>
2025-12-25 14:56:21 +0800

    install tailwindcss, vite, daisyui, fixed header

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
+import tailwindcss from "@tailwindcss/vite";
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
@@ -14,4 +14,7 @@ export default defineConfig({
 			enabled: true,
 		},
 	}),
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,22 @@
 
 export const SITE_TITLE = "Astro Blog";
 export const SITE_DESCRIPTION = "Welcome to my website!";
+
+/**
+ * Site deployment and build configuration
+ */
+export const config = {
+  /** Site's deploy URL - must start with https:// or http:// */
+  url: "https://www.t2del.com/",
+  /** Base path for deployment (e.g. "/blog" for GitHub Pages) */
+  base: "",
+  /** Number of blog posts displayed per page */
+  postsPerPage: 6,
+  projectsPerPage: 8,
+    WP_BLOG_LIST: "https://block.t2del.com/wp-json/wp/v2/posts?_embed",
+    WP_BLOG_POST: "https://block.t2del.com/wp-json/wp/v2/posts",
+  NUM_POSTS_ON_HOMEPAGE: 4,
+  NUM_WORKS_ON_HOMEPAGE: 3,
+  NUM_PROJECTS_ON_HOMEPAGE: 4,
+  NUM_PROJECTS_ON_PROECTS: 8,
+};
